8d2122c8c2a44b18f23944f65758120f758588fb
Bhaskar kumar <thisisofficialbk@gmail.com>
2022-01-09 15:03:08 +0530

    server setup done

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get'pages/home', to:'pages#home'
+  get'pages/about', to:'pages#about'
+end
